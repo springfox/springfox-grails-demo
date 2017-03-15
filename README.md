755da70fe2fc51c46fc13c1a8944bbf016fd65ec
dilipkrish <dilipkrish@users.noreply.github.com>
2017-03-14 22:16:24 -0500

    Updated the documentation and simplified configuration

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # springfox-grails-demo
 Sample application to demonstrate the capabilities of integrating with grails
+
+Details on how to configure the grails app are available in the [library repository](https://github.com/springfox/springfox-grails-integration). You can see this demo running [live here](https://immense-escarpment-17128.herokuapp.com/swagger-ui.html).
--- grails-app/init/hello/world/Application.groovy
+++ grails-app/init/hello/world/Application.groovy
@@ -1,15 +1,12 @@
 package hello.world
 
-import com.fasterxml.classmate.TypeResolver
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
-import grails.core.GrailsApplication
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Import
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
-import springfox.documentation.grails.*
+import springfox.documentation.grails.SpringfoxGrailsIntegrationConfiguration
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
@@ -22,35 +19,6 @@ import static springfox.documentation.builders.PathSelectors.ant
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
         GrailsApp.run(Application, args)
-    }
-
-    @Bean
-    @ConditionalOnMissingBean(GrailsPropertySelector)
-    GrailsPropertySelector propertySelector() {
-        new DefaultGrailsPropertySelector()
-    }
-
-    @Bean
-    @ConditionalOnMissingBean(GrailsPropertyTransformer)
-    GrailsPropertyTransformer propertyTransformer() {
-        new DefaultGrailsPropertyTransformer()
-    }
-
-    @Bean
-    @ConditionalOnMissingBean(GeneratedClassNamingStrategy)
-    GeneratedClassNamingStrategy namingStrategy() {
-        new DefaultGeneratedClassNamingStrategy()
-    }
-
-    @Bean
-    DefaultGrailsAlternateTypeRuleConvention grailsConvention(
-            TypeResolver resolver,
-            GrailsApplication application,
-            GrailsSerializationTypeGenerator generator) {
-        new DefaultGrailsAlternateTypeRuleConvention(
-                resolver,
-                application,
-                generator)
     }
 
     @Bean
