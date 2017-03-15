1a0c5f4d8e724f0710267b00d7b4619ebd8c375a
dilipkrish <dilipkrish@users.noreply.github.com>
2017-03-14 23:43:00 -0500

    Forced conventions

--- grails-app/init/hello/world/Application.groovy
+++ grails-app/init/hello/world/Application.groovy
@@ -9,15 +9,9 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Import
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
-import springfox.documentation.grails.DefaultGeneratedClassNamingStrategy
-import springfox.documentation.grails.DefaultGrailsAlternateTypeRuleConvention
-import springfox.documentation.grails.DefaultGrailsPropertySelector
-import springfox.documentation.grails.DefaultGrailsPropertyTransformer
-import springfox.documentation.grails.GeneratedClassNamingStrategy
-import springfox.documentation.grails.GrailsPropertySelector
-import springfox.documentation.grails.GrailsPropertyTransformer
-import springfox.documentation.grails.GrailsSerializationTypeGenerator
-import springfox.documentation.grails.SpringfoxGrailsIntegrationConfiguration
+import springfox.documentation.grails.*
+import springfox.documentation.schema.AlternateTypeRule
+import springfox.documentation.schema.AlternateTypeRuleConvention
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
@@ -60,14 +54,18 @@ class Application extends GrailsAutoConfiguration {
                 application,
                 generator)
     }
-    
+
     @Bean
-    Docket api() {
+    Docket api(List<AlternateTypeRuleConvention> conventions) {
+        def typeRules = conventions.collectMany {
+            it.rules()
+        }
         new Docket(DocumentationType.SWAGGER_2)
                 .ignoredParameterTypes(MetaClass)
                 .select()
                 .paths(not(ant("/error")))
                 .build()
+                .alternateTypeRules(typeRules.toArray(new AlternateTypeRule[typeRules.size()]))
     }
 
     @Bean
